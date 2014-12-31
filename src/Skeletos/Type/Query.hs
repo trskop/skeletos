bbe4403082aa64010fc798b5f1e314a7661b09bd
Peter Trsko <peter.trsko@ixperta.com>
2014-12-31 20:39:15 +0100

    Query is a list of QueryAtom-s
    Core idea behind this commit is embedded in QueryAtom definition:
    
        data QueryAtom
            = TypeAtom !TemplateType
            | LanguageAtom CIText
            | TagAtom CIText

--- src/Skeletos/Type/Query.hs
+++ src/Skeletos/Type/Query.hs
@@ -13,21 +13,67 @@
 --
 -- TODO
 module Skeletos.Type.Query
---  (
---  )
+    (
+      Query(..)
+    , QueryAtom(..)
+    , queryAtoms
+    , typeAtom
+    , languageAtom
+    , tagAtom
+    )
   where
 
 import Data.Data (Data)
+import Data.Eq (Eq)
+import Data.Function (($))
+import Data.Functor ((<$>))
+import Data.Maybe (Maybe(Just, Nothing))
 import Data.Typeable (Typeable)
 import GHC.Generics (Generic)
 import Text.Read (Read)
 import Text.Show (Show)
 
+import Control.Lens (Lens', Prism', prism')
 import Data.Default.Class (Default(def))
 
+import Skeletos.Type.CIText (CIText)
+import Skeletos.Type.TemplateType (TemplateType)
 
-data Query = Query
-  deriving (Data, Generic, Read, Show, Typeable)
 
+data QueryAtom
+    = TypeAtom !TemplateType
+    | LanguageAtom CIText
+    | TagAtom CIText
+  deriving (Data, Eq, Generic, Read, Show, Typeable)
+
+-- | 'Prism'' for 'TypeAtom'.
+typeAtom :: Prism' QueryAtom TemplateType
+typeAtom = prism' TypeAtom $ \s -> case s of
+    TypeAtom x -> Just x
+    _          -> Nothing
+
+-- | 'Prism'' for 'LanguageAtom'.
+languageAtom :: Prism' QueryAtom CIText
+languageAtom = prism' LanguageAtom $ \s -> case s of
+    LanguageAtom x -> Just x
+    _              -> Nothing
+
+-- | 'Prism'' for 'TagAtom'.
+tagAtom :: Prism' QueryAtom CIText
+tagAtom = prism' TagAtom $ \s -> case s of
+    TagAtom x -> Just x
+    _         -> Nothing
+
+newtype Query = Query [QueryAtom]
+  deriving (Data, Eq, Generic, Read, Show, Typeable)
+
+-- | Defined as:
+--
+-- @
+-- 'def' = 'Query' '[]'
+-- @
 instance Default Query where
-    def = Query
+    def = Query []
+
+queryAtoms :: Lens' Query [QueryAtom]
+queryAtoms f (Query a) = Query <$> f a
