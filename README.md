d225041e562aba8b07a09fe6625b8db75d1f5d5d
Peter Trško <peter.trsko@gmail.com>
2015-01-14 22:17:38 +0100

    Draft of how TUI should look like

--- README.md
+++ README.md
@@ -8,34 +8,72 @@
 
 Create skeleton project, file or code snippet from a template.
 
+> The skeleton (from Greek σκελετός, skeletos "dried up")is the body part that
+> forms the supporting structure of an organism.
+>
+> > -- [Wikipedia: Skeleton][]
+
 
 ## Usage
 
-````
-skeletos {project|file|snippet} [OPTIONS] [--] QUERY
-````
+    skeletos [-D VARIABLE[=[TYPE:]VALUE]] [-o FILE] QUERY
+
+### Variable Definition
+
+    VARIABLE[=[TYPE:]VALUE]
+
+Where:
+
+* `VARIABLE` is arbitrary string that doesn't contain `'='` character.
+
+* `TYPE` is one of `text`, `bool`, `int` or `word`, if omitted, then it will
+  try to guess. Gessing is done as follows:
+
+  1. If `VALUE` is not a negative number, then it is considered a `word`; if
+     it's negative, then it is an `int`.
+  2. If it is `true` or `false` (case ignored) then it is considered a `bool`.
+  3. If non above applies, then it is considered a `text`.
+
+  It is possible to define boolean using numeric value, but then it is
+  necessary to specify a type: `bool:0` (same as `bool:false`) or `bool:42`
+  (same as `bool:true`).
+
+* `VALUE` is arbitrary string that may be constricted by `TYPE`, if defined.
+  `VALUE` is optional and if it's not present, then variable is defined with
+  empty value.
+
+Examples:
+
+    module-name='Data.Foo'
+    module-name='text:Data.Foo'
+    use-tabs='bool:false'
+    shift-width='word:2'
+
+### Query
+
+Query format:
+
+    type={package|file|snippet} [language=STRING] [tag=STRING ...]
 
 
 ## Examples
 
 
+### Create a Haskell Module
+
+````sh
+skeletos
+    -Dmodule-name='Skeletos.Type.Foo' -Dauthor='John Smith' -Demail='em@il' \
+    type=file language=haskell tag=module tag=normal \
+    -o src/Skeletos/Type/Foo.hs
+````
+
+
 ### Create Shell Function
 
-````
-skeletos snippet -Dfunction-name='foo' language=sh tag=function
+````sh
+skeletos -Dfunction-name='foo' type=snippet language=sh tag=function
 ```
-
-````Shell
-# TODO: Description
-#
-# Usage:
-#
-#   foo [OPTIONS]
-function foo()
-{
-    :
-}
-````
 
 
 
@@ -45,3 +83,6 @@ function foo()
 [tl;dr Legal: BSD3]:
   https://tldrlegal.com/license/bsd-3-clause-license-%28revised%29
   "BSD 3-Clause License (Revised)"
+[Wikipedia: Skeleton]:
+  https://en.wikipedia.org/wiki/Skeleton
+  "Skeleton on Wikipedia"
--- src/Main.hs
+++ src/Main.hs
@@ -1,23 +1,129 @@
-{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE RecordWildCards #-}
 -- |
 -- Module:       $HEADER$
 -- Description:  TODO
--- Copyright:    (c) 2014 Peter Trsko
+-- Copyright:    (c) 2014, 2015 Peter Trsko
 -- License:      BSD3
 --
 -- Maintainer:   peter.trsko@gmail.com
 -- Stability:    experimental
--- Portability:  DeriveDataTypeable, NoImplicitPrelude
+-- Portability:  DeriveDataTypeable, NoImplicitPrelude, RecordWildCards
 --
 -- TODO
 module Main (main)
   where
 
-import System.IO (IO)
+import Control.Monad (Monad(return))
+import Data.Either (Either(Left, Right))
+import Data.Function ((.), ($))
+import qualified Data.List as List (null, unwords)
+import Data.Maybe (Maybe(Just, Nothing))
+import Data.Monoid (Monoid(mconcat))
+import Data.String (String)
+import Data.Version (showVersion)
+import System.IO (FilePath, IO, hPutStrLn, print, stderr)
+import Text.Show (Show)
 
+import Data.Text (Text)
+import qualified Data.Text.IO as Text (putStr, writeFile)
+
+import Control.Lens ((.~), (&))
+import Data.Default.Class (Default(def))
+import Options.Applicative
+
+import Skeletos.Parse (parseDefine, parseQueryAtom)
 import Skeletos.Type.Config (Config)
+import qualified Skeletos.Type.Config as Config (defines, query)
+import Skeletos.Type.Define (Define, defines)
+import Skeletos.Type.Query (QueryAtom, queryAtoms)
 
+import Paths_skeletos (version)
+
+
+data OptionsConfig = OptionsConfig
+    { variableDefines :: [Define]
+    , outputFile :: Maybe FilePath
+    -- ^ Is either 'Data.Maybe.Nothing' or 'Data.Maybe.Just' non-empty string.
+    -- Invariant of non-empty string is preserved by 'filePath' parser.
+    , templateSearchQuery :: [QueryAtom]
+    }
+  deriving (Show)
+
+instance Default OptionsConfig where
+    def = OptionsConfig
+        { variableDefines     = []
+        , outputFile          = Nothing
+        , templateSearchQuery = []
+        }
+
+getSkeletosConfig :: OptionsConfig -> Config
+getSkeletosConfig OptionsConfig{..} = def
+    & Config.defines . defines    .~ variableDefines
+    & Config.query   . queryAtoms .~ templateSearchQuery
+
+options :: Parser OptionsConfig
+options = OptionsConfig
+    <$> (many . option (eitherReader parseDefine) . mconcat)
+        [ short 'D'
+        , metavar "VARIABLE[=[TYPE:]VALUE]"
+        , help "Define a variable with optional (typed) value."
+        ]
+    <*> (optional . option filePath . mconcat)
+        [ short 'o'
+        , metavar "FILE"
+        , help $ List.unwords
+            [ "Store output in to a file, if supported by template type,"
+            , "instead of printing it to a stdout."
+            ]
+        ]
+    <*> (some . argument (eitherReader parseQueryAtom))
+        ( metavar "QUERY"
+        <> help (List.unwords
+            [ "Query consisting of one or multiple key=value pairs for"
+            , "selecting specific template."
+            ])
+        )
+  where
+    filePath = nonEmptyStr "Option argument can not be empty file path."
+    nonEmptyStr msg = eitherReader $ \s ->
+        if List.null s
+            then Left msg
+            else Right s
 
 main :: IO ()
-main = return ()
+main = do
+    appConfig <- execParser (info parser infoMod)
+    r <- skeletos $ getSkeletosConfig appConfig
+    case r of
+        Left msg         -> hPutStrLn stderr msg
+        Right Nothing    -> return ()
+        Right (Just out) -> case outputFile appConfig of
+            Nothing   -> Text.putStr out
+            Just file -> Text.writeFile file out
+  where
+    parser = helper <*> versioner <*> options
+
+    infoMod = fullDesc
+        <> progDesc "Create skeleton project, file or code snippet from a template."
+        <> header (List.unwords ["skeletos", versionStr])
+        <> footer (List.unwords
+            [ "QUERY syntax:"
+            , "type={package|file|snippet}"
+            , "[language=STRING]"
+            , "[tag=STRING ...]"
+            ])
+
+    versioner = abortOption (InfoMsg versionStr) $ mconcat
+        [ long "version"
+        , short 'V'
+        , help "Show version number and exit."
+        , hidden
+        ]
+
+    versionStr = showVersion version
+
+    -- Temporary:
+    skeletos :: Config -> IO (Either String (Maybe Text))
+    skeletos = (Right Nothing <$) . print
